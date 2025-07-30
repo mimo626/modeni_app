a54bb2b3c905f72e2fdc3c7ce6eb6e0b79b6a70f
kangminju <yessuk5499@gmail.com>
2025-07-31 02:42:13 +0900

    텍스트스타일 파일 생성

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,364 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle bodyText = const TextStyle(
+    fontSize: 15.0,
+    color: Colors.black54,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle buttonText = const TextStyle(
+    fontSize: 15.0,
+    color: Colors.white,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle bold18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle semiBold32 = const TextStyle(
+    fontSize: 32,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle bold32 = const TextStyle(
+    fontSize: 32,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle bold28 = const TextStyle(
+    fontSize: 28,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle semiBold24 = const TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle bold24 = const TextStyle(
+    fontSize: 24,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+  );
+
+  static TextStyle semiBold22 = const TextStyle(
+    fontSize: 22,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold20 = const TextStyle(
+    fontSize: 20,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle light20 = const TextStyle(
+    fontSize: 20,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle semiBold18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle regular20 = const TextStyle(
+    fontSize: 20,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular8 = const TextStyle(
+    fontSize: 8,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular13 = const TextStyle(
+    fontSize: 13,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle medium18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold16 = const TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle medium16 = const TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle regular16 = const TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular15 = const TextStyle(
+    fontSize: 15,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle light16 = const TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle medium15 = const TextStyle(
+    fontSize: 15,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle semiBold14 = const TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle medium14 = const TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle regular9 = const TextStyle(
+    fontSize: 9,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular10 = const TextStyle(
+    fontSize: 10,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular11 = const TextStyle(
+    fontSize: 11,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle light12 = const TextStyle(
+    fontSize: 12,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle regular12 = const TextStyle(
+    fontSize: 12,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle regular14 = const TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle semiBold13 = const TextStyle(
+    fontSize: 13,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold12 = const TextStyle(
+    fontSize: 12,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold11 = const TextStyle(
+    fontSize: 11,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold10 = const TextStyle(
+    fontSize: 10,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold8 = const TextStyle(
+    fontSize: 8,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle semiBold9 = const TextStyle(
+    fontSize: 9,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle bold11 = const TextStyle(
+    fontSize: 11,
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle bold13 = const TextStyle(
+    fontSize: 13,
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle medium12 = const TextStyle(
+    fontSize: 12,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle medium11 = const TextStyle(
+    fontSize: 11,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle medium10 = const TextStyle(
+    fontSize: 10,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle medium8 = const TextStyle(
+    fontSize: 8,
+    color: Colors.black,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle mediumWithUnderLine12 = const TextStyle(
+      fontSize: 12,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+      decoration: TextDecoration.underline);
+
+  static TextStyle regularInSelectorButton10 = const TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w500,
+    color: Color(0xFF434648),
+  );
+
+  static const TextStyle mediumBlackPretendard12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle regularBlack14 = const TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static TextStyle mediumLightGray12 = const TextStyle(
+    fontSize: 12,
+    color: Color(0xFF595E62),
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle mediumLightGray14 = const TextStyle(
+    fontSize: 12,
+    color: Color(0xFF595E62),
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle bold16 = const TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle gradeTitleTextStyle = const TextStyle(
+    fontSize: 16,
+    color: Colors.black,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle semiBold15 = const TextStyle(
+    fontSize: 15,
+    color: Color(0xFF111111),
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle light14 = const TextStyle(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+
+  static TextStyle light15 = const TextStyle(
+    fontSize: 15,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+
+  static TextStyle light32 = const TextStyle(
+    fontSize: 32,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle light11 = const TextStyle(
+    fontSize: 11,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle light18 = const TextStyle(
+    fontSize: 18,
+    color: Colors.black,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle semiBold28 = const TextStyle(
+    fontSize: 28,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle extraBold8 = const TextStyle(
+    fontSize: 8,
+    color: Colors.black,
+    fontWeight: FontWeight.w800,
+  );
+
+  static TextStyle thin12 = const TextStyle(
+    fontSize: 12,
+    color: Colors.black,
+    fontWeight: FontWeight.w100,
+  );
+}
