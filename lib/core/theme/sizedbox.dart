4c20a79ef26804b4c998e20cf9cde5dba5dc730a
kangminju <yessuk5499@gmail.com>
2025-07-31 02:42:08 +0900

    사이즈박스 파일 생성

---
+++ lib/core/theme/sizedbox.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/widgets.dart';
+
+class AppSizedBox {
+  // width
+  static const SizedBox w2SizedBox = SizedBox(width: 2);
+  static const SizedBox w4SizedBox = SizedBox(width: 4);
+  static const SizedBox w6SizedBox = SizedBox(width: 6);
+  static const SizedBox w8SizedBox = SizedBox(width: 8);
+  static const SizedBox w10SizedBox = SizedBox(width: 10);
+  static const SizedBox w12SizedBox = SizedBox(width: 12);
+  static const SizedBox w16SizedBox = SizedBox(width: 16);
+  static const SizedBox w18SizedBox = SizedBox(width: 18);
+  static const SizedBox w25SizedBox = SizedBox(width: 25);
+  static const SizedBox w32SizedBox = SizedBox(width: 32);
+  static const SizedBox w48SizedBox = SizedBox(width: 48);
+
+  //height
+  static const SizedBox h2SizedBox = SizedBox(height: 2);
+  static const SizedBox h4SizedBox = SizedBox(height: 4);
+  static const SizedBox h6SizedBox = SizedBox(height: 6);
+  static const SizedBox h8SizedBox = SizedBox(height: 8);
+  static const SizedBox h10SizedBox = SizedBox(height: 10);
+  static const SizedBox h12SizedBox = SizedBox(height: 12);
+  static const SizedBox h16SizedBox = SizedBox(height: 16);
+  static const SizedBox h18SizedBox = SizedBox(height: 18);
+  static const SizedBox h20SizedBox = SizedBox(height: 20);
+  static const SizedBox h22SizedBox = SizedBox(height: 22);
+  static const SizedBox h24SizedBox = SizedBox(height: 24);
+  static const SizedBox h28SizedBox = SizedBox(height: 28);
+  static const SizedBox h30SizedBox = SizedBox(height: 30);
+  static const SizedBox h32SizedBox = SizedBox(height: 32);
+  static const SizedBox h34SizedBox = SizedBox(height: 34);
+  static const SizedBox h36SizedBox = SizedBox(height: 36);
+  static const SizedBox h38SizedBox = SizedBox(height: 38);
+  static const SizedBox h40SizedBox = SizedBox(height: 40);
+  static const SizedBox h48SizedBox = SizedBox(height: 48);
+  static const SizedBox h52SizedBox = SizedBox(height: 52);
+  static const SizedBox h54SizedBox = SizedBox(height: 54);
+  static const SizedBox h70SizedBox = SizedBox(height: 70);
+  static const SizedBox h80SizedBox = SizedBox(height: 80);
+  static const SizedBox h86SizedBox = SizedBox(height: 86);
+  static const SizedBox h100SizedBox = SizedBox(height: 100);
+}
