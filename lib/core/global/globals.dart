9a6ed3da0a2409e4c5742a079318acc12dd898ed
kangminju <yessuk5499@gmail.com>
2025-07-31 02:43:40 +0900

    글로벌 파일 생성

---
+++ lib/core/global/globals.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../theme/colors.dart';
+import 'nav_item.dart';
+
+class Globals {
+  // BottomNavigationBarItem의 아이콘 사이즈
+  static double navItemSize = 24;
+
+  // BottomNavigationBarItem들
+  static List<NavItem> navItems = [
+    NavItem(
+      icon: Icon(
+        Icons.home_outlined,
+        color: AppColors.greyColor,
+        size: Globals.navItemSize,
+      ),
+      activeIcon: Icon(
+        Icons.home_outlined,
+        color: AppColors.darkGreyColor,
+        size: Globals.navItemSize,
+      ),
+      label: "홈",
+    ),
+    NavItem(
+      icon: Icon(
+        Icons.menu_outlined,
+        color: AppColors.greyColor,
+        size: Globals.navItemSize,
+      ),
+      activeIcon: Icon(
+        Icons.menu_outlined,
+        color: AppColors.darkGreyColor,
+        size: Globals.navItemSize,
+      ),
+      label: "질문",
+    ),
+    NavItem(
+      icon: Icon(
+        Icons.library_books_outlined,
+        color: AppColors.greyColor,
+        size: Globals.navItemSize,
+      ),
+      activeIcon: Icon(
+        Icons.library_books_outlined,
+        color: AppColors.darkGreyColor,
+        size: Globals.navItemSize,
+      ),
+      label: "일기장",
+    ),
+    NavItem(
+      icon: Icon(
+        Icons.bookmark_outline,
+        color: AppColors.greyColor,
+        size: Globals.navItemSize,
+      ),
+      activeIcon: Icon(
+        Icons.bookmark_outline,
+        color: AppColors.darkGreyColor,
+        size: Globals.navItemSize,
+      ),
+      label: "미션",
+    ),
+    NavItem(
+      icon: Icon(
+        Icons.person_outline,
+        color: AppColors.greyColor,
+        size: Globals.navItemSize,
+      ),
+      activeIcon: Icon(
+        Icons.person_outline,
+        color: AppColors.darkGreyColor,
+        size: Globals.navItemSize,
+      ),
+      label: "MY",
+    ),
+  ];
+}
