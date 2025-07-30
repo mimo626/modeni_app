237be9e36027143c219710ef82f87b0e983d9911
kangminju <yessuk5499@gmail.com>
2025-07-31 02:43:31 +0900

    메인 화면 구현

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:modeni_app/core/theme/text_styles.dart';
+import 'package:modeni_app/features/mission/presentation/pages/mission_page.dart';
+import 'package:modeni_app/features/questions_list/presentation/pages/question_list_page.dart';
+
+import 'core/global/globals.dart';
+import 'core/theme/colors.dart';
+import 'features/diary/presentation/pages/diary_page.dart';
+import 'features/home/presentation/pages/home_page.dart';
+import 'features/mypage/presentation/pages/my_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -7,15 +16,66 @@ class MainPage extends StatefulWidget {
   State<MainPage> createState() => _MainPageState();
 }
 
-class _MainPageState extends State<MainPage> {
+class _MainPageState extends State<MainPage> with TickerProviderStateMixin{
+  late TabController _tabController;
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: Globals.navItems.length, vsync: this,);
+    _tabController.addListener(tabListener);
+  }
+
+  @override
+  void dispose() {
+    _tabController.removeListener(tabListener);
+    super.dispose();
+  }
+
+  void tabListener() {
+    setState(() {
+      _selectedIndex = _tabController.index;
+    });
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _tabController.index = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Center(
-              child: Text("메인 화면")
-          ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: Globals.navItems.map((item) {
+          return BottomNavigationBarItem(
+              icon: item.icon,
+              activeIcon: item.activeIcon,
+              label: item.label
+          );
+        }).toList(),
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedFontSize: 12,
+        onTap: _onItemTapped,
+        backgroundColor: AppColors.whiteColor,
+        selectedItemColor: AppColors.darkGreyColor,
+        unselectedItemColor: AppColors.greyColor,
+        selectedLabelStyle: AppTextStyles.semiBold13,
+        unselectedLabelStyle: AppTextStyles.semiBold13,
+      ),
+      body: TabBarView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: _tabController,
+        children: const [
+          HomePage(),
+          QuestionListPage(),
+          DiaryPage(),
+          MissionPage(),
+          MyPage(),
         ],
       ),
     );
