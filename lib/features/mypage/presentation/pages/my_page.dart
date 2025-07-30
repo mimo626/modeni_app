c52bb366d7efcd8596d4faa8ffe6f93cc6d1dc70
kangminju <yessuk5499@gmail.com>
2025-07-31 03:28:08 +0900

    마이페이지 구현

--- lib/features/mypage/presentation/pages/my_page.dart
+++ lib/features/mypage/presentation/pages/my_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/theme/colors.dart';
+import '../../../../core/widget/primary_app_bar.dart';
+import '../widgets/my_list_widget.dart';
+import '../widgets/my_profile_widget.dart';
+
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
 
@@ -11,11 +16,25 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      appBar: PrimaryAppbar(
+        title: "마이페이지",
+        showNotificationIcon: true,
+        showSettingIcon: true,
+      ),
       body: Column(
         children: [
-          Center(
-              child: Text("마이페이지 화면")
+          MyProfileWidget(),
+          Divider(
+            color: AppColors.lightGreyColor,
+            thickness: 10,
           ),
+          MyListWidget(title: "공지사항", onTap: () {}),
+          MyListWidget(title: "문의하기", onTap: () {}),
+          MyListWidget(title: "자주 묻는 질문", onTap: () {}),
+          MyListWidget(title: "오픈소스 라이선스", onTap: () {}),
+          MyListWidget(title: "버전 정보", onTap: () {}),
+          MyListWidget(title: "로그아웃", onTap: () {}, color: AppColors.greyColor,),
         ],
       ),
     );
