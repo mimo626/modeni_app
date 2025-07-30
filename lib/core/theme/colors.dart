4e59e87b8b625b5ca91404a0b789ba15a8721b17
kangminju <yessuk5499@gmail.com>
2025-07-31 02:41:54 +0900

    컬러 파일 생성

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF8CC67D);
+  static const Color secondaryColor = Color(0xFFD1D2B0);
+  static const Color tertiaryColor = Color(0xFFE7E5D9);
+  static const Color fortyColor = Color(0xFFF9F4F1);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color greyColor = Color(0xFF9BA5B7);
+  static const Color darkGreyColor = Color(0xFF3D3D3D);
+  static const Color blackGreyColor = Color(0xFF2F2F2F);
+  static const Color lightGreyColor = Color(0xFFEEF1F4);
+  static const Color blueColor = Color(0xFF002AFC);
+  static const Color redColor = Color(0xFFFC0032);
+}
