8b48d9f48d8051eb6ca8a06473485a9cd7227544
kangminju <yessuk5499@gmail.com>
2025-07-31 02:43:21 +0900

    마이페이지 화면 생성

---
+++ lib/features/mypage/presentation/pages/my_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+              child: Text("마이페이지 화면")
+          ),
+        ],
+      ),
+    );
+  }
+}
