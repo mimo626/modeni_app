5d00331648f82154fc43ec8bd727e29429f7537d
kangminju <yessuk5499@gmail.com>
2025-07-31 02:42:00 +0900

    패딩 파일 생성

---
+++ lib/core/theme/padding.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/widgets.dart';
+
+class AppPadding {
+  //left
+  static const EdgeInsets left6Padding = EdgeInsets.only(left: 6);
+  static const EdgeInsets left20Padding = EdgeInsets.only(left: 20);
+
+  //top
+  static const EdgeInsets top8Padding = EdgeInsets.only(top: 8);
+  static const EdgeInsets top20Padding = EdgeInsets.only(top: 20);
+  static const EdgeInsets top32Padding = EdgeInsets.only(top: 32);
+  static const EdgeInsets top40Padding = EdgeInsets.only(top: 40);
+
+  //right
+  static const EdgeInsets right0Padding = EdgeInsets.only(right: 0);
+  static const EdgeInsets right8Padding = EdgeInsets.only(right: 8);
+  static const EdgeInsets right9Padding = EdgeInsets.only(right: 9);
+  static const EdgeInsets right12Padding = EdgeInsets.only(right: 12);
+  static const EdgeInsets right15Padding = EdgeInsets.only(right: 15);
+  static const EdgeInsets right16Padding = EdgeInsets.only(right: 16);
+  static const EdgeInsets right20Padding = EdgeInsets.only(right: 20);
+  static const EdgeInsets right24Padding = EdgeInsets.only(right: 24);
+  static const EdgeInsets right28Padding = EdgeInsets.only(right: 28);
+
+
+  //bottom
+  static const EdgeInsets bottom8Padding = EdgeInsets.only(bottom: 8);
+  static const EdgeInsets bottom10Padding = EdgeInsets.only(bottom: 10);
+  static const EdgeInsets bottom12Padding = EdgeInsets.only(bottom: 12);
+  static const EdgeInsets bottom16Padding = EdgeInsets.only(bottom: 16);
+  static const EdgeInsets bottom20Padding = EdgeInsets.only(bottom: 20);
+
+  //horizontal
+  static const EdgeInsets h4Padding = EdgeInsets.symmetric(horizontal: 4);
+  static const EdgeInsets h6Padding = EdgeInsets.symmetric(horizontal: 6);
+  static const EdgeInsets h8Padding = EdgeInsets.symmetric(horizontal: 8);
+  static const EdgeInsets h10Padding = EdgeInsets.symmetric(horizontal: 10);
+  static const EdgeInsets h12Padding = EdgeInsets.symmetric(horizontal: 12);
+  static const EdgeInsets h16Padding = EdgeInsets.symmetric(horizontal: 16);
+  static const EdgeInsets h18Padding = EdgeInsets.symmetric(horizontal: 18);
+  static const EdgeInsets h20Padding = EdgeInsets.symmetric(horizontal: 20);
+  static const EdgeInsets h34Padding = EdgeInsets.symmetric(horizontal: 34);
+
+  //vertical
+  static const EdgeInsets v4Padding = EdgeInsets.symmetric(vertical: 4);
+  static const EdgeInsets v8Padding = EdgeInsets.symmetric(vertical: 8);
+  static const EdgeInsets v10Padding = EdgeInsets.symmetric(vertical: 10);
+  static const EdgeInsets v12Padding = EdgeInsets.symmetric(vertical: 12);
+  static const EdgeInsets v14Padding = EdgeInsets.symmetric(vertical: 14);
+  static const EdgeInsets v16Padding = EdgeInsets.symmetric(vertical: 16);
+  static const EdgeInsets v17Padding = EdgeInsets.symmetric(vertical: 17);
+  static const EdgeInsets v18Padding = EdgeInsets.symmetric(vertical: 18);
+  static const EdgeInsets v20Padding = EdgeInsets.symmetric(vertical: 20);
+  static const EdgeInsets v40Padding = EdgeInsets.symmetric(vertical: 40);
+
+  //horizontalVertical
+  static const EdgeInsets h2v4Padding = EdgeInsets.symmetric(horizontal: 2, vertical: 4);
+  static const EdgeInsets h4v2Padding = EdgeInsets.symmetric(horizontal: 4, vertical: 2);
+  static const EdgeInsets h8v2Padding = EdgeInsets.symmetric(horizontal: 8, vertical: 2);
+  static const EdgeInsets h8v4Padding = EdgeInsets.symmetric(horizontal: 8, vertical: 4);
+  static const EdgeInsets h8v16Padding = EdgeInsets.symmetric(horizontal: 8, vertical: 16);
+  static const EdgeInsets h10v4Padding = EdgeInsets.symmetric(horizontal: 10, vertical: 4);
+  static const EdgeInsets h10v8Padding = EdgeInsets.symmetric(horizontal: 10, vertical: 8);
+  static const EdgeInsets h10v16Padding = EdgeInsets.symmetric(horizontal: 10, vertical: 16);
+  static const EdgeInsets h10v20Padding = EdgeInsets.symmetric(horizontal: 10, vertical: 20);
+  static const EdgeInsets h12v6Padding = EdgeInsets.symmetric(horizontal: 12, vertical: 6);
+  static const EdgeInsets h12v8Padding = EdgeInsets.symmetric(horizontal: 12, vertical: 8);
+  static const EdgeInsets h12v10Padding = EdgeInsets.symmetric(horizontal: 12, vertical: 10);
+  static const EdgeInsets h12v14Padding = EdgeInsets.symmetric(horizontal: 12, vertical: 14);
+  static const EdgeInsets h12v16Padding = EdgeInsets.symmetric(horizontal: 12, vertical: 16);
+  static const EdgeInsets h12v20Padding = EdgeInsets.symmetric(horizontal: 12, vertical: 20);
+  static const EdgeInsets h16v9Padding = EdgeInsets.symmetric(horizontal: 16, vertical: 9);
+  static const EdgeInsets h16v12Padding = EdgeInsets.symmetric(horizontal: 16, vertical: 12);
+  static const EdgeInsets h16v14Padding = EdgeInsets.symmetric(horizontal: 16, vertical: 14);
+  static const EdgeInsets h16v20Padding = EdgeInsets.symmetric(horizontal: 16, vertical: 20);
+  static const EdgeInsets h16v32Padding = EdgeInsets.symmetric(horizontal: 16, vertical: 32);
+  static const EdgeInsets h20v6Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 6);
+  static const EdgeInsets h20v8Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 8);
+  static const EdgeInsets h20v10Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 10);
+  static const EdgeInsets h20v12Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 12);
+  static const EdgeInsets h20v14Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 14);
+  static const EdgeInsets h20v16Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 16);
+  static const EdgeInsets h20v18Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 18);
+  static const EdgeInsets h20v26Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 26);
+  static const EdgeInsets h20v64Padding = EdgeInsets.symmetric(horizontal: 20, vertical: 64);
+  static const EdgeInsets h24v8Padding = EdgeInsets.symmetric(horizontal: 24, vertical: 8);
+  static const EdgeInsets h32v12Padding = EdgeInsets.symmetric(horizontal: 32, vertical: 12);
+
+  //fromLTRB
+  static const EdgeInsets l0t4r0b20Padding = EdgeInsets.fromLTRB(0, 4, 0, 20);
+  static const EdgeInsets l4t4r8b4Padding = EdgeInsets.fromLTRB(4, 4, 8, 4);
+  static const EdgeInsets l4t2r4b4Padding = EdgeInsets.fromLTRB(4, 2, 4, 4);
+  static const EdgeInsets l6t2r8b2Padding = EdgeInsets.fromLTRB(6, 2, 8, 2);
+
+
+  //all
+  static const EdgeInsets all4Padding = EdgeInsets.all(4);
+  static const EdgeInsets all8Padding = EdgeInsets.all(8);
+  static const EdgeInsets all12Padding = EdgeInsets.all(12);
+  static const EdgeInsets all16Padding = EdgeInsets.all(16);
+  static const EdgeInsets all20Padding = EdgeInsets.all(20);
+  static const EdgeInsets all24Padding = EdgeInsets.all(24);
+}
