ae7f8631df128bc720d0e4beaea0bfb5818eed73
kangminju <yessuk5499@gmail.com>
2025-07-31 01:57:20 +0900

    클린 아키텍쳐 폴더 적용

---
+++ lib/main_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+              child: Text("메인 화면")
+          ),
+        ],
+      ),
+    );
+  }
+}
