041b991f19d03292f6ee902d017cb1f8aa62a19d
kangminju <yessuk5499@gmail.com>
2025-08-02 00:33:21 +0900

    일기 화면 수정

--- lib/features/diary/presentation/pages/diary_page.dart
+++ lib/features/diary/presentation/pages/diary_page.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:modeni_app/core/theme/padding.dart';
+import 'package:modeni_app/core/theme/sizedbox.dart';
+import 'package:modeni_app/core/theme/text_styles.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../../../../core/widget/basic_btn.dart';
+import '../../../../core/widget/primary_app_bar.dart';
 
 class DiaryPage extends StatelessWidget {
   const DiaryPage({super.key});
@@ -8,76 +15,27 @@ class DiaryPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text("일기", style: TextStyle(color: Colors.black)),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0,
-        leading: const SizedBox(),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.notifications_none, color: Colors.grey),
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: [
-            // 날짜 선택
-            SizedBox(
-              height: 50,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: List.generate(8, (index) {
-                  final day = index + 1;
-                  final isToday = day == 8;
-                  final isSelected = day == 2;
-                  return isToday
-                      ? CircleAvatar(
-                    radius: 20,
-                    backgroundColor: Colors.green.shade300,
-                    child: Text('$day', style: const TextStyle(color: Colors.white)),
-                  )
-                      : Container(
-                    width: 32,
-                    height: 32,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: isSelected ? Colors.green : Colors.transparent),
-                      shape: BoxShape.circle,
-                    ),
-                    child: Text('$day'),
-                  );
-                }),
-              ),
-            ),
-            const SizedBox(height: 8),
-            const Icon(Icons.expand_more),
-
-            const SizedBox(height: 12),
-            // 작성하기 버튼
-            SizedBox(
+      appBar: PrimaryAppbar(title: "일기", showNotificationIcon: true,),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: AppPadding.h20v26Padding,
+            child: SizedBox(
               width: double.infinity,
-              height: 48,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.green.shade200,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-                ),
+              child: BasicBtn(btnText: "작성하기", textColor: AppColors.whiteColor,
+                backgroundColor: AppColors.tertiaryColor,
                 onPressed: () => context.push("/diary_write"),
-                child: const Text("작성하기", style: TextStyle(color: Colors.white)),
               ),
             ),
-            const Divider(height: 32),
-
-            // 가족 구성원들
-            const DiaryItemRow(name: "아빠", tags: ["뿌듯함", "행복", "산책하기"]),
-            const SizedBox(height: 16),
-            const DiaryItemRow(name: "엄마", tags: ["뿌듯함", "행복", "산책하기"]),
-          ],
-        ),
+          ),
+          // 가족 구성원들
+          const Divider(),
+          const DiaryItemRow(name: "아빠", emotions: ["뿌듯함", "행복",], activities: ["산책하기", "요리하기"], content: ""),
+          AppSizedBox.h16SizedBox,
+          const Divider(),
+          const DiaryItemRow(name: "엄마", emotions: ["뿌듯함", "행복"], activities: ["산책하기", "요리하기"], content: "재밌는 하루였습니당"),
+        ],
       ),
     );
   }
@@ -85,42 +43,54 @@ class DiaryPage extends StatelessWidget {
 
 class DiaryItemRow extends StatelessWidget {
   final String name;
-  final List<String> tags;
+  final List<String> emotions;
+  final List<String> activities;
+  final String content;
 
-  const DiaryItemRow({super.key, required this.name, required this.tags});
+  const DiaryItemRow({super.key,
+    required this.name,
+    required this.emotions,
+    required this.activities,
+    required this.content});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(name, style: const TextStyle(fontWeight: FontWeight.bold)),
-        const SizedBox(height: 8),
-        Wrap(
-          spacing: 8,
-          children: tags.map((tag) => _KeywordChip(tag: tag)).toList(),
-        ),
-        const Divider(height: 32),
-      ],
+    return Padding(
+      padding: AppPadding.h20Padding,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AppSizedBox.h24SizedBox,
+          Text(name, style: const TextStyle(fontWeight: FontWeight.bold)),
+          AppSizedBox.h8SizedBox,
+          Wrap(
+            spacing: 8,
+            children: emotions.map((emotion) => _KeywordChip(tag: emotion, bgColor: AppColors.secondaryColor,)).toList(),
+          ),
+          AppSizedBox.h12SizedBox,
+          Wrap(
+            spacing: 8,
+            children: activities.map((activity) => _KeywordChip(tag: activity, bgColor: AppColors.orangeColor,)).toList(),
+          ),
+          AppSizedBox.h12SizedBox,
+
+          content.trim().isEmpty ? SizedBox() :
+          Text(content, style: AppTextStyles.medium14.copyWith(color: AppColors.darkGreyColor)),
+          AppSizedBox.h16SizedBox,
+        ],
+      ),
     );
   }
 }
 
 class _KeywordChip extends StatelessWidget {
   final String tag;
+  final Color bgColor;
 
-  const _KeywordChip({required this.tag});
+  const _KeywordChip({required this.tag, required this.bgColor});
 
   @override
   Widget build(BuildContext context) {
-    Color bgColor;
-    if (tag == "뿌듯함") {
-      bgColor = Colors.green.shade100;
-    } else if (tag == "행복") {
-      bgColor = Colors.pink.shade100;
-    } else {
-      bgColor = Colors.blue.shade100;
-    }
 
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
