2cf6973295a10e11a201121699fe6d9b9b2d581c
kangminju <yessuk5499@gmail.com>
2025-08-01 20:49:13 +0900

    잘문 페이지 구현

--- lib/features/questions_list/presentation/pages/question_detail_page.dart
+++ lib/features/questions_list/presentation/pages/question_detail_page.dart
@@ -5,6 +5,11 @@ import '../../../../core/theme/padding.dart';
 import '../../../../core/theme/sizedbox.dart';
 import '../../../../core/theme/text_styles.dart';
 import '../../../../core/widget/primary_app_bar.dart';
+import 'package:go_router/go_router.dart';
+
+import '../widgets/familiy_list.dart';
+import '../widgets/question_card.dart';
+
 
 class QuestionDetailPage extends StatefulWidget {
   final int index;
@@ -37,12 +42,12 @@ class _QuestionDetailPageState extends State<QuestionDetailPage> {
                     backgroundColor: AppColors.tertiaryColor,
                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
                   ),
-                  onPressed: () {},
+                  onPressed: () => context.push("/answer_write"),
                   child: Text("작성하러 가기", style: AppTextStyles.medium14.copyWith(color: AppColors.whiteColor)),
                 ),
               ),
             ),
-            const FamilyList(),
+            FamilyList(),
           ],
         ),
       ),
@@ -50,123 +55,4 @@ class _QuestionDetailPageState extends State<QuestionDetailPage> {
   }
 }
 
-class QuestionCard extends StatelessWidget {
-  final int index;
-  final String question;
-  const QuestionCard({super.key, required this.index, required this.question});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      padding: AppPadding.all40Padding,
-      decoration: BoxDecoration(
-        color: AppColors.secondaryColor,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            "${index.toString().padLeft(3, '0')}",
-            style: AppTextStyles.semiBold16.copyWith(color: AppColors.primaryColor),),
-          AppSizedBox.w16SizedBox,
-          Expanded(
-            child: Text(
-              question,
-              style: AppTextStyles.medium14,
-              softWrap: true, 
-              maxLines: 3,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class FamilyList extends StatelessWidget {
-  const FamilyList({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final members = [
-      FamilyMember(name: "엄마", answer: "없어서는 안될 존재없어서는 안될 존재없어서는 안될 존재없어서는 안될 존재없어서는 안될 존재", imgUrl: "lib/core/images/dau_profile.png", heartCount: 3),
-      FamilyMember(name: "아빠", answer: "없어서는 안될 존재", imgUrl: "lib/core/images/dad_profile.png", heartCount: 1),
-    ];
-
-    return Column(
-      children: members.map((member) => FamilyCard(member: member)).toList(),
-    );
-  }
-}
-
-class FamilyMember {
-  final String name;
-  final String answer;
-  final String imgUrl;
-  final int heartCount;
-
-  FamilyMember({required this.name, required this.answer, required this.imgUrl, required this.heartCount, });
-}
-
-class FamilyCard extends StatelessWidget {
-  final FamilyMember member;
-
-  const FamilyCard({super.key, required this.member});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: AppPadding.bottom12Padding,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Container(
-            padding: AppPadding.all20Padding,
-            decoration: BoxDecoration(
-                color: AppColors.whiteColor,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: AppColors.primaryColor)
-            ),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        Image.asset(member.imgUrl, scale: 6,),
-                        AppSizedBox.h4SizedBox,
-                        Text(member.name, style: AppTextStyles.medium14),
-                      ],
-                    ),
-                    AppSizedBox.w16SizedBox,
-                    Expanded(
-                      child: Text(member.answer, style: AppTextStyles.medium14,
-                        softWrap: true,
-                        overflow: TextOverflow.visible,),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.zero,
-                backgroundColor: AppColors.primaryColor,
-                foregroundColor: AppColors.primaryColor,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-              ),
-              child: Text("👍️ ${member.heartCount}", style: AppTextStyles.medium16.copyWith(color: AppColors.whiteColor),),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
--- lib/features/questions_list/presentation/pages/question_list_page.dart
+++ lib/features/questions_list/presentation/pages/question_list_page.dart
@@ -6,7 +6,8 @@ import 'package:modeni_app/core/theme/sizedbox.dart';
 import 'package:modeni_app/core/theme/text_styles.dart';
 import 'package:modeni_app/core/widget/list_view_widget.dart';
 import 'package:modeni_app/core/widget/primary_app_bar.dart';
-import 'package:go_router/go_router.dart';
+
+import '../widgets/question_widget.dart';
 
 class QuestionListPage extends StatefulWidget {
   const QuestionListPage({super.key});
@@ -35,41 +36,5 @@ class _QuestionListPageState extends State<QuestionListPage> {
   }
 }
 
-class QuestionWidget extends StatefulWidget {
-  final int index;
-  final String question;
-  const QuestionWidget({super.key, required this.index, required this.question});
 
-  @override
-  State<QuestionWidget> createState() => _QuestionWidgetState();
-}
-
-class _QuestionWidgetState extends State<QuestionWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if(widget.index == 1) AppSizedBox.h24SizedBox,
-        InkWell(
-          onTap: () => context.push("/question_detail", extra: {"index": widget.index, "question": widget.question}),
-          child: Row(
-            children: [
-              Text(
-                "${widget.index.toString().padLeft(3, '0')}",
-                style: AppTextStyles.semiBold16.copyWith(color: AppColors.primaryColor),),
-              AppSizedBox.w18SizedBox,
-              Expanded(
-                child: Text("${widget.question}", style: AppTextStyles.medium14.copyWith(color: AppColors.darkGreyColor),
-                  softWrap: true,
-                  maxLines: 3,
-                ),
-              ),
-            ],
-          ),
-        ),
-        AppSizedBox.h20SizedBox,
-      ],
-    );
-  }
-}
 
