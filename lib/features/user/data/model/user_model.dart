b806a5ec0aa9f54b6a5b9700def7f3f760f81d82
kangminju <yessuk5499@gmail.com>
2025-08-02 05:44:56 +0900

    유저 모델 수정

--- lib/features/user/data/model/user_model.dart
+++ lib/features/user/data/model/user_model.dart
@@ -1,14 +1,16 @@
 class UserModel {
+  final int id;
   final String user_id;
   final String password;
   final String name;
   final String role;
   final String region;
-  final BigInt family_code;
+  final String family_code;
   final DateTime? created_at;
   final String age;
 
   UserModel({
+    required this.id,
     required this.user_id,
     required this.password,
     required this.name,
@@ -19,8 +21,25 @@ class UserModel {
     required this.age,
   });
 
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as int,
+      user_id: json['user_id'] as String,
+      password: json['password'] as String,
+      name: json['name'] as String,
+      role: json['role'] as String,
+      region: json['region'] as String,
+      family_code: json['family_code'] as String,
+      created_at: json['created_at'] != null
+          ? DateTime.parse(json['created_at'] as String)
+          : null,
+      age: json['age'].toString(), // 혹시 int일 수도 있어서 toString() 처리
+    );
+  }
+
   Map<String, dynamic> toJson() {
     return {
+      'id': id,
       'user_id': user_id,
       'password': password,
       'name': name,
