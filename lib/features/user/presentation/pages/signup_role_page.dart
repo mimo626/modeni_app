a568ebd5580256aa1a6dd283dabe168f8e78b407
kangminju <yessuk5499@gmail.com>
2025-08-01 11:31:27 +0900

    역할 입력 화면 구현

---
+++ lib/features/user/presentation/pages/signup_role_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupRolePage extends StatefulWidget {
+  const SignupRolePage({super.key});
+
+  @override
+  State<SignupRolePage> createState() => _SignupRolePageState();
+}
+
+class _SignupRolePageState extends State<SignupRolePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
