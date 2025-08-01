62d7d0a42e3a4fade7fe64c9e6345cd9efa6388e
kangminju <yessuk5499@gmail.com>
2025-08-02 00:33:28 +0900

    일기 작성 화면 수정

--- lib/features/diary/presentation/pages/diary_write_page.dart
+++ lib/features/diary/presentation/pages/diary_write_page.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:modeni_app/core/global/globals.dart';
 import 'package:modeni_app/core/theme/colors.dart';
+import 'package:modeni_app/core/theme/sizedbox.dart';
+import 'package:modeni_app/core/theme/text_styles.dart';
+import 'package:modeni_app/core/widget/text_field_title.dart';
+
+import '../../../../core/theme/padding.dart';
+import '../../../../core/widget/basic_btn.dart';
+import '../../../../core/widget/primary_app_bar.dart';
+import 'package:go_router/go_router.dart';
 
 class DiaryWritePage extends StatefulWidget {
   const DiaryWritePage({super.key});
@@ -10,43 +19,61 @@ class DiaryWritePage extends StatefulWidget {
 
 class _DiaryWritePageState extends State<DiaryWritePage> {
   final TextEditingController _controller = TextEditingController();
-
-  List<String> emotions = ['뿌듯함', '행복', '행복', '행복', '행복', '행복', '행복', '행복',];
-  List<String> todos = ['산책하기', '요리하기', '요리하기', '산책하기', '요리하기', '요리하기', '산책하기', '요리하기', '요리하기'];
+  bool isButtonEnabled = false;
 
   Set<String> selectedEmotions = {};
   Set<String> selectedTodos = {};
 
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(_updateButtonState);
+  }
+
+  // 변경: 감정 선택 함수
   void _toggleEmotion(String emotion) {
     setState(() {
       if (selectedEmotions.contains(emotion)) {
         selectedEmotions.remove(emotion);
-      } else if (selectedEmotions.length < 2) {
+      } else if (selectedEmotions.length < 3) {
         selectedEmotions.add(emotion);
       }
+      _updateButtonState();
     });
   }
 
+// 변경: 할 일 선택 함수
   void _toggleTodo(String todo) {
     setState(() {
       if (selectedTodos.contains(todo)) {
         selectedTodos.remove(todo);
-      } else if (selectedTodos.length < 2) {
+      } else if (selectedTodos.length < 3) {
         selectedTodos.add(todo);
       }
+      _updateButtonState();
     });
   }
 
+// 추가: 버튼 상태 업데이트 함수
+  void _updateButtonState() {
+    isButtonEnabled = selectedEmotions.length > 0 &&
+        selectedTodos.length > 0 &&
+        selectedEmotions.length <= 3 &&
+        selectedTodos.length <= 3;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
-      appBar: AppBar(
-        leading: const Icon(Icons.close),
-        title: const Text("일기 작성", style: TextStyle(color: Colors.black)),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        elevation: 0,
+      appBar: PrimaryAppbar(title: "일기 작성",
+        leadingButtonIcon: "lib/core/icons/x.svg",
+        leadingButtonPressed: () => context.pop(),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -54,80 +81,78 @@ class _DiaryWritePageState extends State<DiaryWritePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text("오늘의 감정", style: TextStyle(fontWeight: FontWeight.bold)),
-              const Text("키워드를 1-2개 선택해 주세요.", style: TextStyle(fontSize: 12, color: Colors.grey)),
-              const SizedBox(height: 8),
+              Text("오늘 어떤 감정을 느꼈나요?", style: AppTextStyles.semiBold16),
+              AppSizedBox.h4SizedBox,
+              Text("최대 3개 선택 가능", style: AppTextStyles.medium12.copyWith(color: AppColors.greyColor)),
+              AppSizedBox.h8SizedBox,
               Wrap(
                 spacing: 8,
                 runSpacing: 8,
-                children: emotions.map((e) {
+                children: Globals.emotions.map((e) {
                   final isSelected = selectedEmotions.contains(e);
                   return ChoiceChip(
                     label: Text(e),
+                    showCheckmark: false,
+                    backgroundColor: AppColors.lightGreyColor,
                     selected: isSelected,
-                    selectedColor: Colors.green.shade100,
+                    selectedColor: AppColors.secondaryColor,
                     onSelected: (_) => _toggleEmotion(e),
-                    shape: StadiumBorder(side: BorderSide(color: isSelected ? Colors.green : Colors.grey)),
+                    shape: StadiumBorder(side: BorderSide(color: isSelected ?AppColors.secondaryColor : AppColors.lightGreyColor)),
                   );
                 }).toList(),
               ),
               const SizedBox(height: 24),
-              const Text("오늘 하고 싶은 일", style: TextStyle(fontWeight: FontWeight.bold)),
-              const Text("키워드를 1-2개 선택해 주세요.", style: TextStyle(fontSize: 12, color: Colors.grey)),
-              const SizedBox(height: 8),
+              Text("오늘 어떤 걸 하고싶으신가요?", style: AppTextStyles.semiBold16),
+              AppSizedBox.h4SizedBox,
+              Text("최대 3개 선택 가능", style: AppTextStyles.medium12.copyWith(color: AppColors.greyColor)),
+              AppSizedBox.h8SizedBox,
               Wrap(
                 spacing: 8,
                 runSpacing: 8,
-                children: todos.map((t) {
+                children: Globals.activities.map((t) {
                   final isSelected = selectedTodos.contains(t);
                   return ChoiceChip(
                     label: Text(t),
+                    showCheckmark: false,
+                    backgroundColor: AppColors.lightGreyColor,
                     selected: isSelected,
-                    selectedColor: Colors.orange.shade100,
+                    selectedColor: AppColors.orangeColor,
                     onSelected: (_) => _toggleTodo(t),
-                    shape: StadiumBorder(side: BorderSide(color: isSelected ? Colors.orange : Colors.grey)),
+                    shape: StadiumBorder(side: BorderSide(color: isSelected ? AppColors.orangeColor : AppColors.lightGreyColor)),
                   );
                 }).toList(),
               ),
-              const SizedBox(height: 24),
-              const Text("직접 입력", style: TextStyle(fontWeight: FontWeight.bold)),
-              const Text("못다한 말을 직접 적어보세요.", style: TextStyle(fontSize: 12, color: Colors.grey)),
-              const SizedBox(height: 8),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.green.shade100),
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: TextField(
-                  controller: _controller,
-                  maxLength: 50,
-                  maxLines: 5,
-                  decoration: const InputDecoration(
-                    hintText: '50자 이내 입력',
-                    border: InputBorder.none,
-                    counterText: '',
-                  ),
-                ),
+              AppSizedBox.h24SizedBox,
+              Text("오늘 나만의 이야기", style: AppTextStyles.semiBold16),
+              AppSizedBox.h8SizedBox,
+              TextFieldTitle(controller: _controller,
+                  hintText: "최대 50자 이내 입력",
+                  minLines: 5,
               ),
-              const SizedBox(height: 24),
-              SizedBox(
-                width: double.infinity,
-                height: 48,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.green.shade200,
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
-                  ),
-                  onPressed: () {
-                    // 작성 완료 처리
-                  },
-                  child: const Text("작성하기", style: TextStyle(color: Colors.white)),
-                ),
-              ),
+              AppSizedBox.h24SizedBox,
             ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: AppPadding.v16Padding,
+        child: BasicBtn(
+          btnText: "입력 완료",
+          textColor: AppColors.whiteColor,
+          backgroundColor: isButtonEnabled
+              ? AppColors.primaryColor
+              : AppColors.lightGreyColor,
+          onPressed: () {
+            if (isButtonEnabled) {
+              setState(() {
+                // 유저 정보 저장
+                context.pop();
+              });
+            }
+            else {
+              null;
+            }
+          },
         ),
       ),
     );
