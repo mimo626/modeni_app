7c5af7522584bfabae977d12938ceffb99575ec3
kangminju <yessuk5499@gmail.com>
2025-08-02 05:45:23 +0900

    UserController 수정

--- lib/features/user/presentation/controller/user_controller.dart
+++ lib/features/user/presentation/controller/user_controller.dart
@@ -2,15 +2,17 @@ import 'package:get/get.dart';
 import '../../data/model/user_model.dart';
 
 class UserController extends GetxController {
+  final id = 0.obs;
   final user_id = ''.obs;
   final password = ''.obs;
   final name = ''.obs;
   final age = ''.obs;
   final role = ''.obs;
   final region = ''.obs;
-  final family_code = BigInt.zero.obs;
+  final family_code = ''.obs;
 
   UserModel get user => UserModel(
+    id: id.value,
     user_id: user_id.value,
     password: password.value,
     name: name.value,
