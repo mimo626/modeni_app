4be55ff5ed8ff7c56b46eba812a58c059868af7a
kangminju <yessuk5499@gmail.com>
2025-08-01 11:30:56 +0900

    로그인 화면 구현

--- lib/features/user/presentation/pages/login_page.dart
+++ lib/features/user/presentation/pages/login_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:modeni_app/core/theme/colors.dart';
+import 'package:modeni_app/core/theme/padding.dart';
+import 'package:modeni_app/core/theme/sizedbox.dart';
+import 'package:modeni_app/core/widget/basic_btn.dart';
+import 'package:go_router/go_router.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -11,12 +16,28 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.primaryColor,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-              child: Text("로그인 화면")
+            child: Image.asset(
+              "lib/core/images/modeni_logo.png",
+            scale: 1.2,),
           ),
+          //AppSizedBox.h54SizedBox,
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: AppPadding.v16Padding,
+        child: BasicBtn(btnText: "시작하기",
+            textColor: AppColors.primaryColor,
+            backgroundColor: AppColors.whiteColor,
+            onPressed: (){
+              context.push("/signup_name");
+            }
+        ),
       ),
     );
   }
