787a26b401c11ed61af07e3db00af5a82fae918c
kangminju <yessuk5499@gmail.com>
2025-08-01 11:42:09 +0900

    라우터 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:modeni_app/features/user/presentation/pages/signup_name_page.dart';
+import 'package:modeni_app/features/user/presentation/pages/signup_region_page.dart';
+import 'package:modeni_app/features/user/presentation/pages/signup_role_page.dart';
 
 import 'features/user/presentation/pages/login_page.dart';
 import 'main_page.dart';
@@ -34,13 +36,13 @@ final GoRouter _router = GoRouter(
     GoRoute(
       path: '/signup_role',
       builder: (context, state) {
-        return LoginPage();
+        return SignupRolePage();
       },
     ),
     GoRoute(
       path: '/signup_region',
       builder: (context, state) {
-        return LoginPage();
+        return SignupRegionPage();
       },
     ),
   ]
