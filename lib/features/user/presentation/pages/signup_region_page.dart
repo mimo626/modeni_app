d9f116b8fa9f029c3b0c0b87b91ab6e17881db0d
kangminju <yessuk5499@gmail.com>
2025-08-01 11:31:23 +0900

    지역 입력 화면 구현

---
+++ lib/features/user/presentation/pages/signup_region_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupRegionPage extends StatefulWidget {
+  const SignupRegionPage({super.key});
+
+  @override
+  State<SignupRegionPage> createState() => _SignupRegionPageState();
+}
+
+class _SignupRegionPageState extends State<SignupRegionPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
