a17a7321b3349e025b6857032c4e87a8c4baa67c
kangminju <yessuk5499@gmail.com>
2025-08-02 07:44:56 +0900

    가족 답변 리스트 구현

--- lib/features/questions_list/presentation/widgets/familiy_answer_list.dart
+++ lib/features/questions_list/presentation/widgets/familiy_answer_list.dart
@@ -1,42 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:modeni_app/core/global/globals.dart';
 
 import '../../../../core/theme/colors.dart';
 import '../../../../core/theme/padding.dart';
 import '../../../../core/theme/sizedbox.dart';
 import '../../../../core/theme/text_styles.dart';
 
-class FamilyMember {
-  final String name;
-  final String answer;
-  final String imgUrl;
-  final int heartCount;
 
-  FamilyMember({required this.name, required this.answer, required this.imgUrl, required this.heartCount, });
+class Member {
+  final String role; // 엄마, 아빠, 아들, 딸
+  final String answer;
+  int heartCount;
+  Set<String> likedUserIds; // 누가 좋아요 눌렀는지 저장
+
+  Member({
+    required this.role,
+    required this.answer,
+    required this.heartCount,
+    required this.likedUserIds,
+  });
 }
 
-class FamilyAnswerList extends StatelessWidget {
-  const FamilyAnswerList({super.key});
+class FamilyAnswerList extends StatefulWidget {
+  final Member? member;
+  const FamilyAnswerList(this.member, {super.key,});
 
   @override
+  State<FamilyAnswerList> createState() => _FamilyAnswerListState();
+}
+
+class _FamilyAnswerListState extends State<FamilyAnswerList> {
+  final currentUserId = 'user123'; // 예시 사용자 ID
+
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if(widget.member != null) {
+      Globals.members.add(widget.member!);
+    }
+  }
+  @override
   Widget build(BuildContext context) {
-    final members = [
-      FamilyMember(name: "엄마", answer: "없어서는 안될 존재없어서는 안될 존재없어서는 안될 존재없어서는 안될 존재없어서는 안될 존재", imgUrl: "lib/core/images/dau_profile.png", heartCount: 3),
-      FamilyMember(name: "아빠", answer: "없어서는 안될 존재", imgUrl: "lib/core/images/dad_profile.png", heartCount: 1),
-    ];
 
     return Column(
-      children: members.map((member) => FamilyAnswerCard(member: member)).toList(),
+      children: Globals.members
+          .map((member) => FamilyAnswerCard(
+        member: member,
+        currentUserId: currentUserId,
+      ))
+          .toList(),
     );
   }
 }
 
-class FamilyAnswerCard extends StatelessWidget {
-  final FamilyMember member;
 
-  const FamilyAnswerCard({super.key, required this.member});
+class FamilyAnswerCard extends StatefulWidget {
+  final Member member;
+  final String currentUserId; // 현재 로그인한 유저 ID
+
+  const FamilyAnswerCard({super.key, required this.member, required this.currentUserId,});
+
+  @override
+  State<FamilyAnswerCard> createState() => _FamilyAnswerCardState();
+}
+
+class _FamilyAnswerCardState extends State<FamilyAnswerCard> {
+  String _getProfileImage(String role) {
+    switch (role) {
+      case '엄마':
+        return 'lib/core/images/mom_profile.png';
+      case '아빠':
+        return 'lib/core/images/dad_profile.png';
+      case '아들':
+        return 'lib/core/images/son_profile.png';
+      case '딸':
+        return 'lib/core/images/dau_profile.png';
+      default:
+        return 'lib/core/images/default_profile.png';
+    }
+  }
+
+  void _toggleLike() {
+    final userId = widget.currentUserId;
+
+    setState(() {
+      if (widget.member.likedUserIds.contains(userId)) {
+        // 이미 눌렀으면 취소
+        widget.member.likedUserIds.remove(userId);
+        widget.member.heartCount--;
+      } else {
+        // 안 눌렀으면 추가
+        widget.member.likedUserIds.add(userId);
+        widget.member.heartCount++;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final member = widget.member;
+    final profileImage = _getProfileImage(member.role);
+    final isLiked = member.likedUserIds.contains(widget.currentUserId);
+
     return Padding(
       padding: AppPadding.bottom12Padding,
       child: Column(
@@ -55,9 +122,9 @@ class FamilyAnswerCard extends StatelessWidget {
                   children: [
                     Column(
                       children: [
-                        Image.asset(member.imgUrl, scale: 6,),
+                        Image.asset(profileImage, width: 60, height: 60), // 프로필 이미지
                         AppSizedBox.h4SizedBox,
-                        Text(member.name, style: AppTextStyles.medium14),
+                        Text(member.role, style: AppTextStyles.medium14),
                       ],
                     ),
                     AppSizedBox.w16SizedBox,
@@ -72,16 +139,21 @@ class FamilyAnswerCard extends StatelessWidget {
             ),
           ),
           GestureDetector(
-            onTap: () {},
+            onTap: _toggleLike,
             child: ElevatedButton(
-              onPressed: () {},
+              onPressed: _toggleLike,
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.zero,
-                backgroundColor: AppColors.primaryColor,
-                foregroundColor: AppColors.primaryColor,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                backgroundColor: isLiked ? AppColors.primaryColor : AppColors.greyColor,
+                foregroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
-              child: Text("👍️ ${member.heartCount}", style: AppTextStyles.medium16.copyWith(color: AppColors.whiteColor),),
+              child: Text(
+                "👍️ ${member.heartCount}",
+                style: TextStyle(fontSize: 16),
+              ),
             ),
           ),
         ],
