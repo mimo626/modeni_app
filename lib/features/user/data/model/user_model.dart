16d4b2327e465b2271b0b53d789d841f709b4aca
kangminju <yessuk5499@gmail.com>
2025-08-02 07:46:06 +0900

    유저 모델 수정

--- lib/features/user/data/model/user_model.dart
+++ lib/features/user/data/model/user_model.dart
@@ -6,7 +6,6 @@ class UserModel {
   final String role;
   final String region;
   final String family_code;
-  final DateTime? created_at;
   final String age;
 
   UserModel({
@@ -17,36 +16,32 @@ class UserModel {
     required this.role,
     required this.region,
     required this.family_code,
-    this.created_at,
     required this.age,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
-      id: json['id'] as int,
-      user_id: json['user_id'] as String,
-      password: json['password'] as String,
-      name: json['name'] as String,
-      role: json['role'] as String,
-      region: json['region'] as String,
-      family_code: json['family_code'] as String,
-      created_at: json['created_at'] != null
-          ? DateTime.parse(json['created_at'] as String)
-          : null,
-      age: json['age'].toString(), // 혹시 int일 수도 있어서 toString() 처리
+      id: json['id'] as int? ?? 0,
+      user_id: json['userId'] as String? ?? '',
+      password: json['password'] as String? ?? '',
+      name: json['name'] as String? ?? '',
+      role: json['role'] as String? ?? '',
+      region: json['region'] as String? ?? '',
+      family_code: json['familyCode'] as String? ?? '',
+      age: json['age'].toString() ?? '', // 혹시 int일 수도 있어서 toString() 처리
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'user_id': user_id,
+      'userId': user_id,
       'password': password,
       'name': name,
       'age': age,
       'role': role,
       'region': region,
-      'family_code': family_code,
+      'familyCode': family_code,
     };
   }
 }
