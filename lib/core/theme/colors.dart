0a699dab819fea244523243f2dd3757b0f72d483
kangminju <yessuk5499@gmail.com>
2026-01-03 23:52:53 +0900

    색상 코드 수정

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -3,18 +3,12 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF8CC67D);
-  static const Color secondaryColor = Color(0xFFE4F6E0);
-  static const Color tertiaryColor = Color(0xFFC0E0B9);
-  static const Color orangeColor = Color(0xFFF6E8E0);
-  static const Color pinkColor = Color(0xFFF6E0E9);
-  static const Color blueColor = Color(0xFFE0F2F6);
-  static const Color yellowColor = Color(0xFFF6EFE0);
+  static const Color primaryColor = Color(0xFFBFA288);
+  static const Color secondaryColor = Color(0xFFEAEAEA);
+  static const Color tertiaryColor = Color(0xFFF2F2F2);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
-  static const Color greyColor = Color(0xFF9BA5B7);
-  static const Color darkGreyColor = Color(0xFF3D3D3D);
-  static const Color blackGreyColor = Color(0xFF2F2F2F);
-  static const Color lightGreyColor = Color(0xFFEEF1F4);
-  static const Color redColor = Color(0xFFFC0032);
+  static const Color darkGreyColor = Color(0xFF525252);
+  static const Color greyColor = Color(0xFF9F9F9F);
+  static const Color errorColor = Color(0xFFFC0032);
 }
